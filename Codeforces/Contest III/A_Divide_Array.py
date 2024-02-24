74fafb84dbba7cb69fb84628a055363b4d8b52ea
Bruh Lemma Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-02-24 17:36:39 -0500

    FIxed A_Divide_Array. Now works.

--- Codeforces/Contest III/A_Divide_Array.py
+++ Codeforces/Contest III/A_Divide_Array.py
@@ -1,32 +1,34 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-# target
-    # a, b, and c have at least one element
+neg = []
+pos = []
+zero = []
 
-a = [x for x in arr if x < 0]
-b = [x for x in arr if x > 0]
-c = [x for x in arr if x == 0]
-
-if len(a) % 2 == 0:
-    if len(b) == 0:
-        if len(a) >= 2:
-            b.append(a.pop())
-            b.append(a.pop())
-        else:
-            b.append(a.pop())
-            c.append(a.pop())
+for i in arr:
+    if i > 0:
+        pos.append(i)
+    elif i < 0:
+        neg.append(i)
     else:
-        c.append(a.pop())
+        zero.append(i)
 
-if len(b) == 0 and len(a) % 2 == 1:
-    if len(a) >= 2:
-        b.append(a.pop())
-        b.append(a.pop())
+if len(neg) % 2 == 0:
+    # even size
+    if len(neg) == 2:
+        zero.append(neg.pop())
+    elif len(neg) > 3:
+        if len(pos) == 0:
+            pos.append(neg.pop())
+            pos.append(neg.pop())
+            zero.append(neg.pop())
+        else:
+            zero.append(neg.pop())
+if len(pos) == 0 and len(neg) >= 3:
+    pos.append(neg.pop())
+    pos.append(neg.pop())
 
-print(len(a), end=' ')
-print(*a)
-print(len(b), end=' ')
-print(*b)
-print(len(c), end=' ')
-print(*c)
+
+print(len(neg), *neg)
+print(len(pos), *pos)
+print(len(zero), *zero)
