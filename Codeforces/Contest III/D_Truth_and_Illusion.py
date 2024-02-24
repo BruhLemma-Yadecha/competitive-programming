3c92e01b1322ac9d5f0cb3970baa790dd660e193
Bruh Lemma Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-02-24 18:24:11 -0500

    closer to a solution to D

--- Codeforces/Contest III/D_Truth_and_Illusion.py
+++ Codeforces/Contest III/D_Truth_and_Illusion.py
@@ -6,20 +6,46 @@ for _ in range(t):
     A = []
     for _ in range(n):
         A.append(list(input()))
-    
+
     operations = 0
     
-    # Check and adjust diagonal elements
-    for i in range(n):
-        if A[i][i] != A[n-1-i][n-1-i]:
-            operations += 1
-            A[i][i] = A[n-1-i][n-1-i]
+    # generate the four triangles
+    top = []
+    left = []
+    for i in range(n // 2):
+        T = [A[i][x] for x in range(0 + i, n - i)]
+        L = [A[x][i] for x in range(0 + i, n - i)]
+        for val in T:
+            top.append(val)
+        for val in L:
+            left.append(val)
+
+    if n % 2 == 1:
+        top.append(A[n // 2][n // 2])
+        left.append(A[n // 2][n // 2])
     
-    # Adjust upper triangular part to match lower triangular part
-    for i in range(n):
-        for j in range(i + 1, n):
-            if A[i][j] != A[j][i]:
-                operations += 1
-                A[i][j] = A[j][i]
+    bottom = []
+    right = []
+    j = 0
+    for i in range(n - 1, n // 2 - 1, -1):
+        B = [A[i][x] for x in range(0 + j, n - j)]
+        R = [A[x][i] for x in range(0 + j, n - j)]
+        for val in B:
+            bottom.insert(0, val)
+        for val in R:
+            right.insert(0, val)
+        j += 1
+        
     
-    print(operations)
+    # which triangle to enforce
+    top_one = top.count('1')
+    bottom_one = bottom.count('1')
+    left_one = left.count('1')
+    right_one = right.count('1')
+
+    top_changes = abs(top_one - bottom_one) + abs(top_one - left_one) + abs(top_one - right_one)
+    bottom_changes = abs(bottom_one - top_one) + abs(bottom_one - left_one) + abs(bottom_one - right_one)
+    right_changes = abs(right_one - top_one) + abs(right_one - bottom_one) +abs(right_one - left_one)
+    left_changes = abs(left_one - top_one) + abs(left_one - bottom_one) + abs(left_one - right_one)
+    
+    print(min(top_changes, bottom_changes, left_changes, right_changes))
