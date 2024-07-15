4d7802d440a6c3b62cb3a13eedc6fa357b5f2d98
Bruh Lemma <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-07-15 17:35:06 +0400

    Created 'Binary Tree Right Side View' by A2SV Hub

---
+++ 15-Jul-2024/Binary Tree Right Side View 180505.py
@@ -0,0 +1,25 @@
+# Problem: Binary Tree Right Side View - https://leetcode.com/problems/binary-tree-right-side-view/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if not root:
+            return res
+        level = [root]
+        while level:
+            res.append(level[-1].val)
+            next_level = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            level = next_level
+        return res
+                
