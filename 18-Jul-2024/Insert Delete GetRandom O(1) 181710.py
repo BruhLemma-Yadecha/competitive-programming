7b34da3670ad4b14dcf477b6286596396f049277
Bruh Lemma <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-07-19 01:05:45 +0400

    Created 'Insert Delete GetRandom O(1)' by A2SV Hub

---
+++ 18-Jul-2024/Insert Delete GetRandom O(1) 181710.py
@@ -0,0 +1,44 @@
+# Problem: Insert Delete GetRandom O(1) - https://leetcode.com/problems/insert-delete-getrandom-o1/
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.s  = set()
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.s:
+            self.s.add(val)
+            return True
+        else:
+            self.s.add(val)
+        return False
+        
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.s:
+            self.s.remove(val)
+            return True
+        return False
+        
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.sample(self.s, 1)[0]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
