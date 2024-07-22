39180bc91e16b41f8553e10fdd42510ecb01dc1c
Bruh Lemma <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-07-22 17:43:29 +0400

    Created 'A - Hit the Lottery' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/A - Hit the Lottery 182765.py
@@ -0,0 +1,13 @@
+# Problem: A - Hit the Lottery - https://codeforces.com/gym/536373/problem/A
+
+n = int(input())
+
+denominations = [100, 20, 10, 5, 1]
+
+bills = 0
+for d in denominations:
+    local_bills = n // d
+    bills += local_bills
+    n -= d * local_bills
+    
+print(bills)
