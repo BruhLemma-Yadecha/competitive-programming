c1828c96f3d20abe8ef5938c5dbca9b7d7099e1e
Bruh Lemma <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-07-22 17:43:39 +0400

    Created 'B - Remove Smallest' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/B - Remove Smallest 182766.py
@@ -0,0 +1,14 @@
+# Problem: B - Remove Smallest - https://codeforces.com/gym/536373/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    valid = True
+    for i in range(n - 1):
+        diff = abs(a[i] - a[i + 1])
+        if diff > 1:
+            valid = False
+            break
+    print('YES' if valid else 'NO')
