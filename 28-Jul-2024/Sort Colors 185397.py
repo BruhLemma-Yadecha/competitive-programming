86dc3b0291974b6d070e7033a9b178758915a087
Bruh Lemma <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-07-28 20:14:09 +0400

    Created 'Sort Colors' by A2SV Hub

---
+++ 28-Jul-2024/Sort Colors 185397.py
@@ -0,0 +1,19 @@
+# Problem: Sort Colors - https://leetcode.com/problems/sort-colors/
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i, j, k = 0, 0, len(nums) - 1
+        while j <= k:
+            if nums[j] == 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                i += 1
+                j += 1
+            elif nums[j] == 1:
+                j += 1
+            else:
+                nums[j], nums[k] = nums[k], nums[j]
+                k -= 1
+        return
